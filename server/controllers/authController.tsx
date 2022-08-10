137808d2421300087daf626aa6a21c133f66833a
alexarobertss <alexarobertss@protonmail.com>
2022-08-10 00:47:25 -0400

    combining frontend and backend

--- server/controllers/authController.tsx
+++ server/controllers/authController.tsx
@@ -4,7 +4,21 @@ const salt = bcrypt.genSaltSync(10)
 
 
 module.exports = {
-
+  //checking if email exists in database, if so get stored password
+  getUserInfo: async (req, res, next) => {
+    const {email} = req.body
+    try {
+      const data = await db.query('SELECT * FROM Users where email = $1', [email])
+      res.locals.dataStore = data.rows[0]
+      if (!data.rows.length) {
+        alert('No User Found');
+      }
+      return next(); 
+    } catch (err) {
+      return next({err:'error in getUserInfo'})
+    }
+  
+  }, 
   // middleware for checking if the password of a given username matches the encrypted version of said password
   verifyUser: async (req, res, next) => {
     const { inputPassword } = req.body
@@ -16,24 +30,24 @@ module.exports = {
         return next()
       })
       .catch((err) => {
-        return next(err)
+        return next({err:'error in verifyUser'})
       });
   },
 
 
   // middleware for adding a new user to the db through signup
   createUser: async (req, res, next) => {
-    const { firstname, lastname, username, inputPassword } = req.body
-    const user = await db.query('SELECT username FROM users WHERE username = $1', [username])
+    const { firstName, lastName, email, password } = req.body
+    const user = await db.query('SELECT email FROM users WHERE email = $1', [email])
     console.log(user.rows)
     //if user doesnt exist, hash the password
     if (user.rows <= 0) {
-      bcrypt.hash(inputPassword, salt, function (err, hash) {
+      bcrypt.hash(password, salt, function (err, hash) {
         if (err) {
           console.log('whoops')
           return next(err)
         }
-        db.query('INSERT INTO users (firstname, lastname, username, password) VALUES ($1, $2, $3, $4)', [firstname, lastname, username, hash])
+        db.query('INSERT INTO Users (firstName, lastname, email, password) VALUES ($1, $2, $3, $4)', [firstName, lastName, email, hash])
         return next()
       })
     } else {
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,51 @@
-
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/SignUp.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+const useInput = (initial) => {
+  const [value, setValue] = useState(initial)
+  const onChange = (e) => {
+    setValue(e.target.value)
+  }
+  return [value, onChange];
+}
 
 function SignUp() {
+
+  const [firstName, firstNameOnChange] = useInput('');
+  const [lastName, lastNameOnChange] = useInput('');
+  const [email, emailOnChange] = useInput('');
+  const [password, passwordOnChange] = useInput('');
+
+  const navigate = useNavigate()
+  const navigateToProfile = () => {
+    navigate('/Profile')
+  }
+  const createUser = () => {
+    const body = {
+      email,
+      password,
+      firstName,
+      lastName
+    }
+
+    fetch('/signup', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+      })
+      .then((data) => {
+        console.log(data);
+        if(data) navigateToProfile();
+        else alert('Wrong Email or Password');
+      })
+      .catch((error) => console.log(error))
+  }
+  
+  
+
   return (
     <div className='signUp-container'>
         <div class="top"></div>
@@ -14,21 +56,21 @@ function SignUp() {
           SignUp
         </header>
         <label className='firstTextField'>
-          <input type="text" name="firstName" placeholder="First Name"/>
+          <input type="text" name="firstName" placeholder="First Name" onChange={firstNameOnChange}/>
         </label>
         <label className='lastTextField'>
-          <input type="text" name="lastName" placeholder="Last Name"/>
+          <input type="text" name="lastName" placeholder="Last Name" onChange={lastNameOnChange}/>
         </label>
-        <label className='userTextField'>
+        {/* <label className='userTextField'>
           <input type="text" name="userName" placeholder="Username"/>
-        </label>
+        </label> */}
         <label className='emailTextField'>
-          <input type="text" name="emailField" placeholder='Email'/> 
+          <input type="text" name="emailField" placeholder='Email'onChange={emailOnChange}/> 
         </label>
         <label className='passwordTextField'>
-          <input type="text" name="passwordField" placeholder='Password'/> 
+          <input type="password" name="passwordField" placeholder='Password'onChange={passwordOnChange}/> 
         </label>
-        <button id='signUp-button' > <Link to='/login'> Sign Up </Link> </button>
+        <button id='signUp-button' onClick={createUser}> Sign Up </button>
         <div id='logIn-link'>
           <Link to='/login'> Have an account? Log in! </Link>
         </div>
